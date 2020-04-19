9941d7ac36f76f1b47d38d8aa2c05cfcce7480d4
choi94 <ehdgns94@icloud.com>
2020-04-19 21:39:05 +0900

    'commit'

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Frame from './frame/Frame.jsx';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Frame/>
+      </div>
+    );
+  }
+}
+
+export default App;
